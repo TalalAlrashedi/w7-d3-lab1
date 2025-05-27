9dfe584703618f2fc3c82f143b93868ed9fdcf0e
TalalAlrashedi <TalalAlrashedi24@hotmail.com>
2025-05-27 11:01:33 +0300

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState([]);
 
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+    // console.log("loop");
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-gray-100 min-h-screen p-8">
+      <div className="mx-auto">
+        <h2 className="text-3xl font-bold text-center mb-8">
+          المنتجات الرقمية
+        </h2>
+
+        <div className="grid grid-cols-1 relative sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 ">
+          {items.map((i) => (
+            <div
+              key={i.id}
+              className="bg-white p-4 rounded shadow hover:shadow-lg transition"
+            >
+              <p className="text-sm w-30 bg-sky-900 text-white py-1 rounded rounded-2xl mb-3 text-center">{i.category}</p>
+              <img
+                src={i.image}
+                alt={i.title}
+                className="h-40 w-full object-contain mb-4"
+              />
+              <h3 className="text-[18px] text-center p-2 lg:text-2xl font-bold">{i.title}</h3>
+              <p className="text-sm mb-4">{i.description}</p>
+            <hr />
+            <p className="text-green-700"> {i.price}$</p>
+            </div>
+          ))}
+        </div>
+   
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
